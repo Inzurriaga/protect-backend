f20ea146fb930681a8dc7fcda841bce67ee8133e
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-09-12 16:35:10 -0600

    server recieve info from app, update the local data, and send data to website

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 var socket = require('socket.io');
 
-app.locals.title = 'Pet Box';
+app.locals.users = [];
+app.locals.title = "hello im the backend"
 
 app.use(express.json());
 
@@ -20,7 +21,25 @@ app.get('/', (request, response) => {
 
 io.on('connection', function(socket){
   console.log("a user connected");
-  socket.on("what", (msg) => {
-    console.log(msg)
+  socket.on("what", function(data){
+    updateUser(data)
+    io.emit("what", app.locals.users)
   })
 });
+
+// this is for user position and update
+
+function updateUser(data) {
+  const { users } = app.locals
+  const userCheck = users.find( user => user.id === data.id)
+  if(!userCheck){
+    users.push(data)
+  }else{
+    users.map(user => {
+      if(user.id === data.id){
+        return data
+      }
+      return user
+    })
+  }
+}
